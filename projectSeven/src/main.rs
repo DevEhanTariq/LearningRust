aa18efa6ef85843611250a647c9da564fac0702c
DevET <ehan.tariq.21@icloud.com>
2026-07-05 17:04:31 +0100

    Feat: Finished Rust basics

--- projectSeven/src/main.rs
+++ projectSeven/src/main.rs
@@ -1,3 +1,12 @@
 fn main() {
+    let mut n: u128 = 100;
+    loop {
+        println!("Counter: {}", n);
+        n = n - 1;
+        if n == 0 {
+            break;
+        }
+    }
+
     println!("Hello, world!");
 }
