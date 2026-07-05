cc7578747a9a42e5c26d88c274af3ca9869d22ca
DevET <ehan.tariq.21@icloud.com>
2026-07-05 15:59:51 +0100

    Feat: Finished Project Five

--- projectFive/src/main.rs
+++ projectFive/src/main.rs
@@ -9,4 +9,14 @@ fn main() {
     */
     let condition: bool = (! false) && true;
     println!("condition = {}", condition);
+
+    let month: &str = "February";
+
+    if month == "January" {
+        println!("The month is January!");
+    } else if month == "February" {
+        println!("The month is February!");
+    } else {
+        println!("The month is not January or February!");
+    }
 }
