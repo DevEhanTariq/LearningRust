b7a68242aaed3843a33e6661872af119f525fe8b
DevET <ehan.tariq.21@icloud.com>
2026-07-05 16:28:16 +0100

    Feat: Finished project six

--- projectSix/src/main.rs
+++ projectSix/src/main.rs
@@ -1,3 +1,31 @@
+use std::io; // Crates can be at the top
+
+fn test() {
+    println!("This is a test function!");
+}
+
+fn add(a: i128, b: i128) -> i128 {
+    a + b // No semi-colon means value gets returned
+}
+
+fn input() -> String {
+    let mut input = String::new();
+    io::stdin()
+        .read_line(&mut input)
+        .expect("Failed to read line");
+    return input; // Can also just write return
+}
+
 fn main() {
     println!("Hello, world!");
+    test();
+    println!("a + b = {}", add(1, 2));
+
+    let number = {
+        let x = 3;
+        x + 1 // No semi-colon means value gets returned
+    };
+    println!("The number is {}", number);
+
+    println!("Hi, {}!", input().trim());
 }
